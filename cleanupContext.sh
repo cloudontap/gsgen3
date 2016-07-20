9cc66f0bb2c2238e46665a397bef78ce66b7f9aa
Michael Leditschke <ml019.ammd.com.au>
2016-07-20 18:43:21 +1000

    Make cleanup more general - don't include file endings

--- cleanupContext.sh
+++ cleanupContext.sh
@@ -3,10 +3,10 @@
 if [[ -n "${GSGEN_DEBUG}" ]]; then set ${GSGEN_DEBUG}; fi
 
 if [[ (-z "${GSGEN_DEBUG}") && (-n "${ROOT_DIR}") ]]; then
-    find ${ROOT_DIR} -name "aggregate_*.json" -delete
+    find ${ROOT_DIR} -name "aggregate_*" -delete
     find ${ROOT_DIR} -name "STATUS.txt" -delete
-    find ${ROOT_DIR} -name "stripped_*.json" -delete
-    find ${ROOT_DIR} -name "temp_*.json" -delete
+    find ${ROOT_DIR} -name "stripped_*" -delete
+    find ${ROOT_DIR} -name "temp_*" -delete
     find ${ROOT_DIR} -name "ciphertext*" -delete
 fi
 
