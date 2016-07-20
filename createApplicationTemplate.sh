efbe3d593e460e470fcb6bad6c223015adc12762
Michael Leditschke <ml019.ammd.com.au>
2016-07-20 12:12:36 +1000

    Correct the type being passed through to the generic template creation script

--- createApplicationTemplate.sh
+++ createApplicationTemplate.sh
@@ -3,6 +3,6 @@
 BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
-${BIN_DIR}/createTemplate.sh -t account "$@"
+${BIN_DIR}/createTemplate.sh -t application "$@"
 RESULT=$?
 
