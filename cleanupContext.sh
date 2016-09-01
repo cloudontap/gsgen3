5318306bda3f4486f0966b7266c899816e378511
Michael Leditschke <ml019@ammd.com.au>
2016-09-01 14:22:08 +1000

    Use return not exit for correct behaviour when sourcing the cleanup script

--- cleanupContext.sh
+++ cleanupContext.sh
@@ -3,7 +3,7 @@
 if [[ -n "${GSGEN_DEBUG}" ]]; then set ${GSGEN_DEBUG}; fi
 
 # Context cleanup is only done from the script that set the context
-if [[ -z "${GSGEN_CONTEXT_DEFINED_LOCAL}" ]]; then exit; fi
+if [[ -z "${GSGEN_CONTEXT_DEFINED_LOCAL}" ]]; then return 0; fi
 
 if [[ (-z "${GSGEN_DEBUG}") && (-n "${ROOT_DIR}") ]]; then
     find ${ROOT_DIR} -name "composite_*" -delete
