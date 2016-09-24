e14901aa727d71b90d5a229d0573c7ba6a4418a0
Michael Leditschke <ml019@ammd.com.au>
2016-09-24 10:12:45 +1000

    Determine buckets from existing account stack

--- syncAccountBuckets.sh
+++ syncAccountBuckets.sh
@@ -6,35 +6,30 @@ trap '. ${BIN_DIR}/cleanupContext.sh; exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGT
 
 function usage() {
     echo -e "\nSynchronise the contents of the code and credentials buckets to the local values" 
-    echo -e "\nUsage: $(basename $0) -a CHECK_AID -d DOMAIN -x -y"
+    echo -e "\nUsage: $(basename $0) -q -x -y"
     echo -e "\nwhere\n"
-    echo -e "(m) -a CHECK_AID is the tenant account id e.g. \"env01\""
-    echo -e "(o) -d DOMAIN is the domain of the buckets to be synchronised"
     echo -e "    -h shows this text"
+    echo -e "(o) -q for no check (quick) - don't check bucket access before attmepting to synchronise"
     echo -e "(o) -x for no delete - by default files in the buckets that are absent locally are deleted"
     echo -e "(o) -y for a dryrun - show what will happen without actually transferring any files"
     echo -e "\nDEFAULTS:\n"
-    echo -e "DOMAIN = {AID}.gosource.com.au"
     echo -e "\nNOTES:\n"
-    echo -e "1. The CHECK_AID is only used to ensure we are in the AID directory"
     echo -e ""
     exit
 }
 
 DRYRUN=
 DELETE="--delete"
+CHECK="true"
 
 # Parse options
-while getopts ":a:d:hy" opt; do
+while getopts ":hqxy" opt; do
     case $opt in
-        a)
-            CHECK_AID=$OPTARG
-            ;;
-        d)
-            DOMAIN=$OPTARG
-            ;;
         h)
             usage
+            ;;
+        q)
+            CHECK=
             ;;
         x)
             DELETE=
@@ -56,34 +51,44 @@ done
 # Set up the context
 . ${BIN_DIR}/setContext.sh
 
-if [[ "${CHECK_AID}" != "${AID}" ]]; then
-echo -e "\nThe provided AID (${CHECK_AID}) doesn't match the root directory (${AID}). Nothing to do."
+if [[ ! ("root" =~ ${LOCATION}) ]]; then
+    echo -e "\nNeed to be in the root directory. Nothing to do."
     usage
 fi
 
+# Locate the bucket names
+CODE_BUCKET=$(cat ${COMPOSITE_STACK_OUTPUTS} | jq -r ".[] | select(.OutputKey==\"s3XaccountXcode\") | .OutputValue | select(.!=null)")
+CREDENTIALS_BUCKET=$(cat ${COMPOSITE_STACK_OUTPUTS} | jq -r ".[] | select(.OutputKey==\"s3XaccountXcredentials\") | .OutputValue | select(.!=null)")
+
+if [[ (-z "${CODE_BUCKET}") ||
+        (-z "${CREDENTIALS_BUCKET}") ||
+        (-z "${ACCOUNT_REGION}") ]]; then
+    echo -e "\nBuckets don't appear to have been created. Maybe create the Account stack first?"
+    usage
+fi
 pushd ${ACCOUNT_DIR}  > /dev/null 2>&1
 
-if [[ -z "${DOMAIN}" ]]; then DOMAIN=${AID}.gosource.com.au; fi
-
 # Confirm access to the code bucket
-aws --region ${REGION} s3 ls s3://code.${DOMAIN}/ > /dev/null 2>&1
-RESULT=$?
-if [[ "$RESULT" -ne 0 ]]; then
-      echo -e "\nCan't access the code bucket. Does the service role for the server include access to the \"${AID}\" configuration bucket? If windows, is a profile matching the account been set up? Nothing to do."
-      usage
+if [[ "${CHECK}" == "true" ]]; then
+    aws --region ${ACCOUNT_REGION} s3 ls s3://${CODE_BUCKET}/ >/dev/null 2>&1
+    RESULT=$?
+    if [[ "$RESULT" -ne 0 ]]; then
+          echo -e "\nCan't access the code bucket. Does the service role for the server include access to the \"${AID}\" configuration bucket? If windows, is a profile matching the account been set up? Nothing to do."
+          usage
+    fi
 fi
-
 cd ${INFRASTRUCTURE_DIR}/startup
-aws --region ${REGION} s3 sync ${DRYRUN} ${DELETE} --exclude=".git*" bootstrap/ s3://code.${DOMAIN}/bootstrap/
+aws --region ${ACCOUNT_REGION} s3 sync ${DRYRUN} ${DELETE} --exclude=".git*" bootstrap/ s3://${CODE_BUCKET}/bootstrap/
 
 # Confirm access to the credentials bucket
-aws --region ${REGION} s3 ls s3://credentials.${DOMAIN}/ > /dev/null 2>&1
-RESULT=$?
-if [[ "$RESULT" -ne 0 ]]; then
-      echo -e "\nCan't access the credentials bucket. Does the service role for the server include access to the \"${AID}\" configuration bucket? If windows, is a profile matching the account been set up? Nothing to do."
-      usage
+if [[ "${CHECK}" == "true" ]]; then
+    aws --region ${ACCOUNT_REGION} s3 ls s3://${CREDENTIALS_BUCKET}/ >/dev/null 2>&1
+    RESULT=$?
+    if [[ "$RESULT" -ne 0 ]]; then
+          echo -e "\nCan't access the credentials bucket. Does the service role for the server include access to the \"${AID}\" configuration bucket? If windows, is a profile matching the account been set up? Nothing to do."
+          usage
+    fi
 fi
-
 cd ${ACCOUNT_CREDENTIALS_DIR}/alm/docker
-aws --region ${REGION} s3 sync ${DRYRUN} ${DELETE} . s3://credentials.${DOMAIN}/${AID}/alm/docker/
+aws --region ${ACCOUNT_REGION} s3 sync ${DRYRUN} ${DELETE} . s3://${CREDENTIALS_BUCKET}/${AID}/alm/docker/
 RESULT=$?
