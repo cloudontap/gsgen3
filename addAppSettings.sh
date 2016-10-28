20387fbef549729a1ddcf39469e743807fa47a10
Michael Leditschke <ml019@ammd.com.au>
2016-10-28 16:19:56 +1100

    Add option to copy appsettings from an existing segment

--- addAppSettings.sh
+++ addAppSettings.sh
@@ -6,25 +6,29 @@ trap '. ${BIN_DIR}/cleanupContext.sh; exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGT
 
 function usage() {
     echo -e "\nAdd the application settings for a segment"
-    echo -e "\nUsage: $(basename $0) -u"
+    echo -e "\nUsage: $(basename $0) -s SEGMENT -u"
     echo -e "\nwhere\n"
     echo -e "    -h shows this text"
+    echo -e "(o) -s SEGMENT if details should be copied from an existing segment"
     echo -e "(o) -u if details should be updated"
     echo -e "\nDEFAULTS:\n"
     echo -e "\nNOTES:\n"
-    echo -e "1. Any application settings are located via the solution"
-    echo -e "   pattern"
-    echo -e "2. Nothing is done if no solution pattern can be found"
+    echo -e "1. If no existing segment is provided, application settings are "
+    echo -e "   located via the solution pattern. Nothing is done if no "
+    echo -e "   solution pattern can be found"
     echo -e "3. The script must be run in the segment directory"
     echo -e ""
     exit
 }
 
 # Parse options
-while getopts ":hu" opt; do
+while getopts ":hs:u" opt; do
     case $opt in
         h)
             usage
+            ;;
+        s)
+            COPY_SEGMENT="$OPTARG"
             ;;
         u)
             UPDATE_APPSETTINGS="true"
@@ -59,30 +63,36 @@ if [[ -n ${SLICES} ]]; then
     fi
 fi
 
-# Find the solution name
-SOLUTION_NAME=$(cat ${COMPOSITE_BLUEPRINT} | jq -r ".Solution.Pattern | select(.!=null)")
-
-if [[ -z "${SOLUTION_NAME}" ]]; then
-    echo -e "\nNo solution pattern configured yet. Maybe try adding the solution first?"
-    usage
+if [[ -n "${COPY_SEGMENT}" ]]; then
+    COPY_DIR="${APPSETTINGS_DIR}/${COPY_SEGMENT}"
+else
+    # Find the solution name
+    SOLUTION_NAME=$(cat ${COMPOSITE_BLUEPRINT} | jq -r ".Solution.Pattern | select(.!=null)")
+    
+    if [[ -z "${SOLUTION_NAME}" ]]; then
+        echo -e "\nNo solution pattern configured yet. Maybe try adding the solution first?"
+        usage
+    fi
+    
+    # Check if a corresponding solution pattern exists
+    PATTERN_DIR="${BIN_DIR}/patterns/solutions/${SOLUTION_NAME}"
+    if [[ ! -d ${PATTERN_DIR} ]]; then
+        echo -e "\nNo pattern found matching the solution name \"${SOLUTION_NAME}\". Nothing to do"
+        RESULT=0
+        exit
+    fi
+    COPY_DIR="${PATTERN_DIR}/appsettings"
 fi
 
-# Check if a corresponding solution pattern exists
-PATTERN_DIR="${BIN_DIR}/patterns/solutions/${SOLUTION_NAME}"
-if [[ ! -d ${PATTERN_DIR} ]]; then
-    echo -e "\nNo pattern found matching the solution name \"${SOLUTION_NAME}\". Nothing to do"
-    RESULT=0
-    exit
-fi
-if [[ ! -d ${PATTERN_DIR}/appsettings ]]; then
-    echo -e "\nNo application settings for the solution pattern. Nothing to do"
+if [[ ! -d ${COPY_DIR} ]]; then
+    echo -e "\nNo application settings found in ${COPY_DIR}. Nothing to do"
     RESULT=0
     exit
 fi
 
 # Copy across the application settings 
 mkdir -p ${SEGMENT_APPSETTINGS_DIR}
-cp -rp ${PATTERN_DIR}/appsettings/* ${SEGMENT_APPSETTINGS_DIR}
+cp -rp ${COPY_DIR}/* ${SEGMENT_APPSETTINGS_DIR}
 
 # All good
 RESULT=0
