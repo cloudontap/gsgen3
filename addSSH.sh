f5e5245cbffd1a853430b448cd7d11d3576040c5
Michael Leditschke <mike@ammd.com.au>
2016-11-16 18:16:38 +1100

    Add additional sanity checks on SSH creation

--- addSSH.sh
+++ addSSH.sh
@@ -41,14 +41,27 @@ INFRASTRUCTURE_DIR="${ROOT_DIR}/infrastructure/${PRODUCT}"
 CREDENTIALS_DIR="${INFRASTRUCTURE_DIR}/credentials"
 if [[ "product" =~ ${LOCATION} ]]; then
     SSH_ID="${PRODUCT}"
+    KEYID=$(cat ${COMPOSITE_STACK_OUTPUTS} | jq -r '.[] | select(.OutputKey=="cmkXproductXcmk") | .OutputValue | select (.!=null)')
 elif [[ "segment" =~ ${LOCATION} ]]; then
     CREDENTIALS_DIR="${CREDENTIALS_DIR}/${SEGMENT}"
     SSH_ID="${PRODUCT}-${SEGMENT}"
+    KEYID=$(cat ${COMPOSITE_STACK_OUTPUTS} | jq -r '.[] | select(.OutputKey=="cmkXsegmentXcmk") | .OutputValue | select (.!=null)')
+    SSHPERSEGMENT=$(cat ${COMPOSITE_BLUEPRINT} | jq -r '.Segment.SSHPerSegment | select (.!=null)')
+    if [[ "${SSHPERSEGMENT}" != "true" ]]; then
+        echo -e "\nAn SSH key is not required for this segment. Check the SSHPerSegment setting if unsure"
+        usage
+    fi
 else
     echo -e "\nWe don't appear to be in the product or segment directory. Are we in the right place?"
     usage
 fi
-    
+
+# Ensure we've create a cmk to encrypt the SSH private key
+if [[ -z "${KEYID}" ]]; then
+    echo -e "\nNo cmk defined to encrypt the SSH private key. Create the cmk slice before running this script again"
+    usage
+fi
+
 # Create an SSH certificate at the product level
 . ${BIN_DIR}/createSSHCertificate.sh ${CREDENTIALS_DIR}
 
