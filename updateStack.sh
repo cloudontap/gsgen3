2afc8f897a5f68c7c4a015cf890672e5bc48d65e
Michael Leditschke <ml019@ammd.com.au>
2016-11-23 11:50:57 +1100

    Automatically manage create/update decision for a stack

--- updateStack.sh
+++ updateStack.sh
@@ -5,16 +5,16 @@ trap '. ${GENERATION_DIR}/cleanupContext.sh; exit ${RESULT:-1}' EXIT SIGHUP SIGI
 
 DELAY_DEFAULT=30
 function usage() {
-    echo -e "\nUpdate an existing CloudFormation stack" 
+    echo -e "\nCreate/Update a CloudFormation stack"
     echo -e "\nUsage: $(basename $0) -t TYPE -s SLICE -u -m -d DELAY -r REGION\n"
     echo -e "\nwhere\n"
-    echo -e "(o) -d DELAY is the interval between checking the progress of stack update"
+    echo -e "(o) -d DELAY is the interval between checking the progress of stack operation"
     echo -e "    -h shows this text"
-    echo -e "(o) -m (MONITOR ONLY) monitors but does not initiate the stack update process"
-    echo -e "(o) -r REGION is the AWS region identifier for the region in which the stack should be updated"
+    echo -e "(o) -i (INITIATE ONLY) initiates but does not monitor the stack operation"
+    echo -e "(o) -m (MONITOR ONLY) monitors but does not initiate the stack operation"
+    echo -e "(o) -r REGION is the AWS region identifier for the region in which the stack should be managed"
     echo -e "(o) -s SLICE is the slice of the solution to be included in the template"
     echo -e "(m) -t TYPE is the stack type - \"account\", \"product\", \"segment\", \"solution\" or \"application\""
-    echo -e "(o) -u (UPDATE ONLY) initiates but does not monitor the stack update process"
     echo -e "\nDEFAULTS:\n"
     echo -e "DELAY = ${DELAY_DEFAULT} seconds"
     echo -e "\nNOTES:\n"
@@ -22,16 +22,17 @@ function usage() {
     echo -e "2. REGION is only relevant for the \"product\" type, where multiple product stacks are necessary"
     echo -e "   if the product uses resources in multiple regions"  
     echo -e "3. \"segment\" is now used in preference to \"container\" to avoid confusion with docker"
+    echo -e "4. If stack isn't defined, create it otherwise update it"
     echo -e ""
     exit
 }
 
 DELAY=${DELAY_DEFAULT}
-UPDATE=true
-WAIT=true
+STACK_INITIATE=true
+STACK_MONITOR=true
 
 # Parse options
-while getopts ":d:hmr:s:t:u" opt; do
+while getopts ":d:himr:s:t:" opt; do
     case $opt in
         d)
             DELAY=$OPTARG
@@ -39,8 +40,11 @@ while getopts ":d:hmr:s:t:u" opt; do
         h)
             usage
             ;;
+        i)
+            STACK_MONITOR=false
+            ;;
         m)
-            UPDATE=false
+            STACK_INITIATE=false
             ;;
         r)
             REGION=$OPTARG
@@ -50,9 +54,6 @@ while getopts ":d:hmr:s:t:u" opt; do
             ;;
         t)
             TYPE=$OPTARG
-            ;;
-        u)
-            WAIT=false
             ;;
         \?)
             echo -e "\nInvalid option: -$OPTARG" 
@@ -75,11 +76,24 @@ if [[ ! -f "$TEMPLATE" ]]; then
     usage
 fi
 
-if [[ "${UPDATE}" = "true" ]]; then
+if [[ "${STACK_INITIATE}" = "true" ]]; then
+    # Compress the template to avoid aws cli size limitastions
     cat $TEMPLATE | jq -c '.' > stripped_${TEMPLATE}
-    aws --region ${REGION} cloudformation update-stack --stack-name $STACKNAME --template-body file://stripped_${TEMPLATE} --capabilities CAPABILITY_IAM
+
+    # Determine required operation
+    STACK_OPERATION="UPDATE"
+    STACK_CLI_COMMAND="create-stack"
+    aws --region ${REGION} cloudformation describe-stacks --stack-name $STACKNAME > $STACK 2>/dev/null
     RESULT=$?
-    if [ "$RESULT" -ne 0 ]; then exit; fi
+    if [[ "$RESULT" -eq 0 ]]; then
+        STACK_OPERATION="CREATE"
+        STACK_CLI_COMMAND="update-stack"
+    fi
+    
+    # Initiate the required operation
+    aws --region ${REGION} cloudformation ${STACK_CLI_COMMAND} --stack-name $STACKNAME --template-body file://stripped_${TEMPLATE} --capabilities CAPABILITY_IAM
+    RESULT=$?
+    if [[ "$RESULT" -ne 0 ]]; then exit; fi
 fi
 
 RESULT=1
@@ -88,12 +102,12 @@ if [[ "${WAIT}" = "true" ]]; then
         aws --region ${REGION} cloudformation describe-stacks --stack-name $STACKNAME > $STACK
         grep "StackStatus" $STACK > STATUS.txt
         cat STATUS.txt
-        grep "UPDATE_COMPLETE" STATUS.txt >/dev/null 2>&1
+        grep "${STACK_OPERATION}_COMPLETE" STATUS.txt >/dev/null 2>&1
         RESULT=$?
-        if [ "$RESULT" -eq 0 ]; then break;fi
-        grep "UPDATE_IN_PROGRESS" STATUS.txt  >/dev/null 2>&1
+        if [[ "$RESULT" -eq 0 ]]; then break;fi
+        grep "${STACK_OPERATION}_IN_PROGRESS" STATUS.txt  >/dev/null 2>&1
         RESULT=$?
-        if [ "$RESULT" -ne 0 ]; then break;fi
+        if [[ "$RESULT" -ne 0 ]]; then break;fi
         sleep $DELAY
     done
 fi
